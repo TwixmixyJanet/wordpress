3c7d7d9e4faf9512e613dda0a75b0b0070f4fd95
Janet Webster <twixmixy@gmail.com>
2023-07-25 17:50:40 -0400

    Final commit today maybe??

--- wordpress/wp-content/themes/skillcrushstarter/category.php
+++ wordpress/wp-content/themes/skillcrushstarter/category.php
@@ -17,7 +17,6 @@ get_header(); ?>
 
 <section class="category-page">
 	<div class="main-content">
-		<?php if ( have_posts()) : ?>
 			<?php if ( have_posts() ) : ?>
 	<?php while ( have_posts() ) : the_post(); ?>
 		<?php get_template_part( 'content', get_post_format() ); ?>
